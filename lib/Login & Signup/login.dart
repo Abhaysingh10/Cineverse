e1b9549dd68867a194919d7c36fa6489890ddc13
abhaysingh10 <1995.abhaysingh@gmail.com>
2022-04-28 21:02:06 +0530

    login page UI completed.

--- lib/Login & Signup/login.dart
+++ lib/Login & Signup/login.dart
@@ -1,5 +1,5 @@
 import 'package:cineverse/Utils/Colors.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,6 +12,142 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: backgroundColor);
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Material(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextFormField(
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: textFormFieldBackgroundColor),
+                        borderRadius: BorderRadius.circular(4.0)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: textFormFieldBackgroundColor),
+                        borderRadius: BorderRadius.circular(4.0)),
+                    filled: true,
+                    fillColor: textFormFieldBackgroundColor,
+                    hintText: "Enter your phone",
+                    hintStyle: const TextStyle(fontFamily: "Poppins"))),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "or",
+            style: TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: .0,
+          ),
+          const Text(
+            "connect with",
+            style: TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 13,
+                fontWeight: FontWeight.normal),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Container(
+              height: 60.0,
+              decoration: BoxDecoration(
+                  color: textFormFieldBackgroundColor,
+                  borderRadius: BorderRadius.circular(4.0)),
+              child: Row(children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: Image.network(
+                    "https://img.icons8.com/color/48/000000/google-logo.png",
+                    scale: 1.5,
+                  ),
+                ),
+                const Text("GOOGLE",
+                    style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 114, 114, 114)))
+              ]),
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4.0),
+                color: textFormFieldBackgroundColor,
+              ),
+              height: 60.0,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Image.network(
+                      "https://img.icons8.com/fluency/48/000000/facebook-new.png",
+                      scale: 1.5,
+                    ),
+                  ),
+                  const Text(
+                    "FACEBOOK",
+                    style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 18.0,
+                        color: Color.fromARGB(255, 114, 114, 114)),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: redColor, borderRadius: BorderRadius.circular(4.0)),
+              margin: const EdgeInsets.only(top: 50),
+              height: 55.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(left: 50.0),
+                    child: Text(
+                      "Continue",
+                      style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 38.0),
+                    child: Text(
+                      ">",
+                      style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
--- lib/Utils/Colors.dart
+++ lib/Utils/Colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const backgroundColor = Colors.white;
 const redColor = Color(0xffF20544);
+const textFormFieldBackgroundColor = Color.fromARGB(255, 232, 232, 232);
