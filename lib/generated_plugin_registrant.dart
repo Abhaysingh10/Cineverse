0c727901c1d8db09d4f11dab49649308a48481e1
abhaysingh10 <1995.abhaysingh@gmail.com>
2022-05-06 23:31:48 +0530

    facebook_auth

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_facebook_auth_web/flutter_facebook_auth_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:smart_auth/smart_auth_web.dart';
 
@@ -21,6 +22,7 @@ void registerPlugins(final Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  FlutterFacebookAuthPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   SmartAuthWeb.registerWith(registrar);
   registrar.registerMessageHandler();
